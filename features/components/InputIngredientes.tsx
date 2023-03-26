f05c952c6279da620c548db480afab383628df68
Barbi Torres <barbi.mtorres@gmail.com>
2023-03-26 22:40:05 +0200

    add reset field in InputIngredientes

--- features/components/InputIngredientes.tsx
+++ features/components/InputIngredientes.tsx
@@ -2,17 +2,30 @@ import React from "react";
 import InputField from "./InputField";
 import logo from "../../public/logo.png";
 import Image from "next/image";
+import { useFormContext } from "react-hook-form";
 
 const InputIngredientes = () => {
+  // Utilizando useFormContext accedemos a los métodos
+  // reset y getValues
+  const { reset, getValues } = useFormContext();
+
+  const onReset = () => {
+    // El método getValues nos devuelve los
+    // ultimos valores de cada input
+    const currentValues = getValues();
+
+    // Al invocar al método reset, le pasamos los valores
+    // actuales y solo reseteamos el de ingredientes
+    reset({ ...currentValues, ingredientes: "" });
+  };
   return (
     <div>
       <label>Ingredientes:</label>
       <div className="ingredientesDiv">
-        {/* En este caso, ya no necesitamos recibir y pasar las
-props al componente <InputField/>*/}
         <InputField hideLabel name="ingredientes" type="text" />
         <button type="button" className="removeButton">
-          <Image src={logo} alt="tacho-basura" />
+          {/* Agregamos el callback para el evento onClick */}
+          <Image src={logo} alt="tacho-basura" onClick={onReset} />
         </button>
       </div>
       <br />
